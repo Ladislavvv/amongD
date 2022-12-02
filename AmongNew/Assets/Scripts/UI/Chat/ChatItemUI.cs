d26bd4cd970bd95768fccc93ff063b346058764f
Ladislavvv <zhukovdev@mail.ru>
2022-12-02 06:18:04 +0300

    fix: chat, nameSync

--- AmongNew/Assets/Scripts/UI/Chat/ChatItemUI.cs
+++ AmongNew/Assets/Scripts/UI/Chat/ChatItemUI.cs
@@ -5,10 +5,13 @@ public class ChatItemUI : MonoBehaviour {
 
     [SerializeField] private Text _text;
     [SerializeField] private Image _image;
+    [SerializeField] public Text _playerName;
 
-    public void Initialize(string text, Color color) {
+    public void Initialize(string text, Color color, string name) {//, string name
         _text.text = text;
         _image.color = color;
+        _playerName.text = name;
+
     }
 
 }
--- AmongNew/Assets/Scripts/UI/Chat/ChatWindowUI.cs
+++ AmongNew/Assets/Scripts/UI/Chat/ChatWindowUI.cs
@@ -9,6 +9,7 @@ public class ChatWindowUI : MonoBehaviourPun {
 
     [SerializeField] private InputField _inputText;
     public PlayerInfo _playerInfo;
+    //public Text _playerName;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Return)) {
@@ -26,25 +27,27 @@ public class ChatWindowUI : MonoBehaviourPun {
         if (string.IsNullOrEmpty(_inputText.text)) { return; }
         if (_playerInfo == null) { return; }
 
-        photonView.RPC("ReceiveMessageRPC", RpcTarget.All, _inputText.text, _playerInfo.CurrentColor.r, _playerInfo.CurrentColor.g, _playerInfo.CurrentColor.b);
-        
+        photonView.RPC("ReceiveMessageRPC", RpcTarget.All, _inputText.text, _playerInfo.CurrentColor.r, _playerInfo.CurrentColor.g, _playerInfo.CurrentColor.b, _playerInfo._playerName.text);
+        //
+        //_playerName.text = PhotonNetwork.LocalPlayer.NickName;
+        //
         _inputText.text = string.Empty;
         _inputText.ActivateInputField();
     }
 
-    private void InstantiateChatItem(string text, Color color) {
+    private void InstantiateChatItem(string text, Color color, string _playerName) {
         ChatItemUI newChatItem = Instantiate(_chatItemPrefab);
 
         newChatItem.transform.SetParent(_container);
         newChatItem.transform.position = Vector3.zero;
         newChatItem.transform.localScale = Vector3.one;
 
-        newChatItem.Initialize(text, color);
+        newChatItem.Initialize(text, color, _playerName);
     }
 
     [PunRPC]
-    public void ReceiveMessageRPC(string text, float red, float green, float blue) {
-        InstantiateChatItem(text, new Color(red, green, blue));
+    public void ReceiveMessageRPC(string text, float red, float green, float blue, string _playerName) {
+        InstantiateChatItem(text, new Color(red, green, blue), _playerName);
     }
 
     public void OnSendButtonPressed() {
